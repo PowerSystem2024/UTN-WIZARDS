ed785a14e1dbe7028778f31688f75c389b3030fa
Victor Alejandro Aguilar <victoralejandroaguilar46@gmail.com>
2024-08-28 17:40:33 -0300

    Agregue la clase 2 de Java

---
+++ AguilarVictorAlejandro/java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
@@ -0,0 +1,14 @@
+
+package Clases;
+/*
+clasee es una plantilla, posse un nombre, posee atributos y metodos
+un ojheto es una instancia de una clase.
+Pesona(Clase)-> Armando (Objeto)
+             -> Karla (Objeto)
+*/
+
+public class Persona {
+    public static void main(String[] args) {
+        
+    }
+}
