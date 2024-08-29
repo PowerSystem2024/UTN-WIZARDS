9e6896e74cdeb87bb20d04f28f7a422e8c48b08c
Victor Alejandro Aguilar <victoralejandroaguilar46@gmail.com>
2024-08-29 00:22:56 -0300

    Agregue la clase 3 Java con ejercicios 3,4,5

--- AguilarVictorAlejandro/java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
+++ AguilarVictorAlejandro/java/Leccion3/LeccionClasesObjetos/src/Clases/Persona.java
@@ -6,9 +6,28 @@ un ojheto es una instancia de una clase.
 Pesona(Clase)-> Armando (Objeto)
              -> Karla (Objeto)
 */
-
+/*
+Persona
+----------------------------------
+ATRIBUTOS
+nombre: String
+genero: char
+ocupacion: String
+------------------------------------
+MÉTODOS
+obtenerNombre(): String
+obtenerGenero(): String
+obtenerOcupacion():  String
+modificarNombre(nombre: String)
+modificarGenenro(genero: String)
+modificarOcupacion(ocupacion: String)
+*/
 public class Persona {
     public static void main(String[] args) {
-        
+        //Stributos de Clase (Caracteristicas)
+        /*
+        String nombre;
+        String apellido;
+        */
     }
 }
