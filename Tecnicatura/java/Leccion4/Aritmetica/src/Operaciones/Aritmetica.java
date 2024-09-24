b89e7046eac3325ad18f22f831eb4f855a1ac329
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-09-24 00:36:14 -0300

    6.1 Sobrecarga de métodos
    Sobrecarga de métodos

--- Tecnicatura/java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Tecnicatura/java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -6,9 +6,33 @@ public class Aritmetica {
     int a;
     int b;
     
+    //El construcotr es un metodo especial
+    public Aritmetica(){ //Constructor 1 Vacio
+        System.out.println("Se esta ejecutando contructor numero uno");
+    }
+    
+    //Estamos viendo lo que se llama sobrecarga de constructores
+    public Aritmetica(int a, int b){ // Constructor 2
+        this.a = a;
+        this.b = b;
+        System.out.println("Se esta ejecutando este constructor numero dos");
+    }
+            
     //Metodo
     public void sumarNumeros(){
         int resutaldo = a + b;
         System.out.println("resultado = " + resutaldo);
+    }
+    
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return a + b;
+    }
+
+    int sumarConArgumentos(int a, int b) {
+        this.a = a;
+        this.b = b;
+        //return a + b;
+        return this.sumarConRetorno();
     }
 }
