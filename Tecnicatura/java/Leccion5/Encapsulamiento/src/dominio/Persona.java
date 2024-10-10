1a10988acce8bc867620869f9bad8e26cc29cdd4
Victor Alejandro Aguilar <victoralejandroaguilar46@gmail.com>
2024-10-09 22:42:23 -0300

    Agregue clase 7 java

--- Tecnicatura/java/Leccion5/Encapsulamiento/src/dominio/Persona.java
+++ Tecnicatura/java/Leccion5/Encapsulamiento/src/dominio/Persona.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//CLASE 7
 package dominio;
 
-/**
- *
- * @author Aguil
- */
+
 public class Persona {
+    //Atributos
+    private String nombre;
+    private double sueldo;
+    private boolean elimminado;
+    
+    //Constructor
+    public Persona(String nombre, double sueldo, boolean eliminado){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.elimminado = eliminado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isElimminado() {
+        return elimminado;
+    }
+
+    public void setElimminado(boolean elimminado) {
+        this.elimminado = elimminado;
+    }
+    
     
 }
