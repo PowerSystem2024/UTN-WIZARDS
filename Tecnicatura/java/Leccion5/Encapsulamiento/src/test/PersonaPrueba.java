858c2eb7ee57a46490889f5b528f0816b5da1c2e
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-14 21:42:05 -0300

    Java - 8.3 Método toString
    Java - 8.3 Método toString

--- Tecnicatura/java/Leccion5/Encapsulamiento/src/test/PersonaPrueba.java
+++ Tecnicatura/java/Leccion5/Encapsulamiento/src/test/PersonaPrueba.java
@@ -8,6 +8,7 @@ import dominio.Persona;
 public class PersonaPrueba {
     public static void main(String[] args) {
         Persona persona1 =  new Persona ("Osvaldo", 57.000, false);
+        System.out.println(" persona1 = " + persona1);
         System.out.println("persona1 su nombre es: " + persona1.getNombre());
         //Modificar a traves de los metodos
         persona1.setNombre("Juan Ignacio");
@@ -17,5 +18,6 @@ public class PersonaPrueba {
         System.out.println("persona1 para obtener el booleano: " + persona1.isEliminado());
         //Tarea: Crear otro objeto de tipo persona, asignar valores de manera inicial 
         //y imprimir, luego modificar su valores y volver  a imprimir
+        System.out.println("persona1 = " + persona1);
     }
 }
