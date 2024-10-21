1c7285c3fcff07020eed1e31d98f299622413248
Victor Alejandro Aguilar <victoralejandroaguilar46@gmail.com>
2024-10-21 17:51:18 -0300

    Agregue clase 9 Java con Herencia

---
+++ Tecnicatura/java/Leccion6/Herencia/src/domain/Empleado.java
@@ -0,0 +1,46 @@
+//CLASE 9
+
+package domain;
+
+public class Empleado extends Persona { //la clase empleado es hija de la clase persona
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados; // Es para incrementar
+    
+    //Constructor
+
+    public Empleado(String nombre, double sueldo) {
+        super (nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    public Empleado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
