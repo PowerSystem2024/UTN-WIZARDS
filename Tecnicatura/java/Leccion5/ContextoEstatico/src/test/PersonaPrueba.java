5de98c90a0197b152c82d149fbd442931238386e
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-22 20:21:07 -0300

    Java - 8.6 Contexto estático práctica Parte 2 Ejecutar con debug File
    Java - 8.6 Contexto estático práctica Parte 2 Ejecutar con debug File

---
+++ Tecnicatura/java/Leccion5/ContextoEstatico/src/test/PersonaPrueba.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package test;
+
+import domain.Persona;
+
+/**
+
+ */
+public class PersonaPrueba {
+    public static void main(String[] args) {
+                Persona perosona1 = new Persona("Ariel");
+                System.out.println("perosona1 = " + perosona1);
+                Persona persona2 = new Persona("Naty");
+                System.out.println("persona2 = " + persona2);
+    }
+    
+}
