c111ac4b4b0aeee06a13f65036883f499d3b5a12
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-28 23:34:58 -0300

    Java - 9.1 Comenzamos, crear una nueva carpeta Lección6: estamos en herencia parte 1
    Java - 9.1 Comenzamos, crear una nueva carpeta Lección6: estamos en herencia parte 1

---
+++ Tecnicatura/java/Leccion6/Herencia/src/domain/Persona.java
@@ -0,0 +1,70 @@
+/*
+
+ */
+package domain;
+
+/**
+
+ */
+public class Persona {
+    //Artibutos de herencia
+    private String nombre;
+    private char genero;
+    private int edad;
+    private String direccion;
+    
+    //Consteector vacia: este es para crear objetos sin necesidad de inicializar
+    //los atributos de la clase
+    public Persona(){ //Constructor 1
+        
+    }
+    public Persona(String nombre) { //Constructor 2
+        this.nombre = nombre;
+    }
+
+    public Persona(String nombre, char genero, int edad, String direccion) { //Constructor 3
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", genero=" + genero + ", edad=" + edad + ", direccion=" + direccion + '}';
+    }
+    
+    
+}
