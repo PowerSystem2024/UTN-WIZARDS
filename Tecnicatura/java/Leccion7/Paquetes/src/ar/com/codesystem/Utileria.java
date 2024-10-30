d843d4d5630cbca49a8e82f6c2a79b587b2a8464
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-30 20:38:07 -0300

    Java - 10.3 Paquetes Parte 1 y Parte 2: también lo hacemos en carpeta Leccion7
    Java - 10.3 Paquetes Parte 1 y Parte 2: también lo hacemos en carpeta Leccion7

---
+++ Tecnicatura/java/Leccion7/Paquetes/src/ar/com/codesystem/Utileria.java
@@ -0,0 +1,15 @@
+/*
+
+ */
+package ar.com.codesystem;
+
+/**
+ *
+ * @author AMD
+ */
+public class Utileria {
+    public static void imprimir(String s){
+        System.out.println("s = " + s);
+    }
+    
+}
