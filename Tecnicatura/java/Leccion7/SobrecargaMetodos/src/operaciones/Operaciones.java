bc0dfc1edb77595bc4f0ef243e285ba5bb315883
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-30 18:51:10 -0300

    Java - 10.2 Sobrecarga de Métodos: Lo hacemos en carpeta Leccion7
    Java - 10.2 Sobrecarga de Métodos: Lo hacemos en carpeta Leccion7

---
+++ Tecnicatura/java/Leccion7/SobrecargaMetodos/src/operaciones/Operaciones.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package operaciones;
+
+/**
+
+ */
+public class Operaciones {
+    public static int sumar(int a, int b){
+        System.out.println("Metodo pare sumar entreos");
+        return a+b;
+    }
+    
+    public static double sumar(double a, double b){
+        System.out.print("Metodo para sumar double");
+        return a + b;
+    }
+}
