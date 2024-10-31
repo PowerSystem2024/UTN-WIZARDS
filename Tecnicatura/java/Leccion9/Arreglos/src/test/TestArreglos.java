dfdbda63832360c2464adc3591136bf11858ff44
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-31 19:53:25 -0300

    Java - 11.1 Arreglos Parte 1: Creamos la carpeta Leccion9
    Java - 11.1 Arreglos Parte 1: Creamos la carpeta Leccion9

---
+++ Tecnicatura/java/Leccion9/Arreglos/src/test/TestArreglos.java
@@ -0,0 +1,14 @@
+/*
+
+ */
+package test;
+
+/**
+
+ */
+public class TestArreglos {
+    public static void main(String[] args) { // lado derec. intaciamos un objeto de tipo object
+        int edades[] = new int [3];  //el lado izq. declaramos la variable
+        System.out.println("edades =" + edades);
+    }
+}
