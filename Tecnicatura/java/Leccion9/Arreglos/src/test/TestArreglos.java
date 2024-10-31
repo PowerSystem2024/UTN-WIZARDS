212460337c21cc945e2d590f86a9ca9316f8f6c8
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-10-31 20:00:24 -0300

    Java - 11.2 Arreglos Parte 2
    Java - 11.2 Arreglos Parte 2

--- Tecnicatura/java/Leccion9/Arreglos/src/test/TestArreglos.java
+++ Tecnicatura/java/Leccion9/Arreglos/src/test/TestArreglos.java
@@ -10,5 +10,8 @@ public class TestArreglos {
     public static void main(String[] args) { // lado derec. intaciamos un objeto de tipo object
         int edades[] = new int [3];  //el lado izq. declaramos la variable
         System.out.println("edades =" + edades);
+        
+        edades[0] = 17;
+        System.out.println("edades 0 = " + edades[0]);
     }
 }
