328ffd08bbb15bc70ebaeb487fcaf2107a2293b2
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-11-02 22:40:31 -0300

    Java - 12.1 Manejo de Matrices Parte 1: Aquí creamos la carpeta Leccion10
    Java - 12.1 Manejo de Matrices Parte 1: Aquí creamos la carpeta Leccion10

---
+++ Tecnicatura/java/Leccion10/Matices/src/test/testMatrices.java
@@ -0,0 +1,13 @@
+/*
+
+ */
+package test;
+
+
+public class testMatrices {
+    public static void main(String[] args){
+        int edades [] [] = new int[3] [2];
+        System.out.println("edades = " + edades);
+    }
+    
+}
