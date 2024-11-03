0b98d78562e9d3e4ba1231fe279ea0806c45a1f4
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-11-02 22:46:11 -0300

    Java - 12.2 Manejo de Matrices Parte 2: Hacer la tarea
    Java - 12.2 Manejo de Matrices Parte 2: Hacer la tarea

--- Tecnicatura/java/Leccion10/Matices/src/test/testMatrices.java
+++ Tecnicatura/java/Leccion10/Matices/src/test/testMatrices.java
@@ -8,6 +8,19 @@ public class testMatrices {
     public static void main(String[] args){
         int edades [] [] = new int[3] [2];
         System.out.println("edades = " + edades);
+        edades [0] [0] = 5; //Lenado manual
+        edades [0] [1] = 7; // Es una diferente columna
+        edades [1] [0] = 8;
+        edades [1] [1] = 4;
+        edades [2] [0] = 2; // Terminamos la tarea
+        edades [2] [1] = 9;
+        
+        System.out.println("edades 0-0 = " + edades [0] [0]);
+        System.out.println("edades 0-1 = " + edades [0] [1]);
+        System.out.println("edades 1-0 = " + edades [1] [0]);
+        System.out.println("edades 1-1 = " + edades [1] [1]);
+        System.out.println("edades 2-0 = " + edades [2] [0]);
+        System.out.println("edades 2-1 = " + edades [2] [1]);
     }
     
 }
