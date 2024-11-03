cf43b8c496ee93e94d5f3a7226084e9f0d8ca081
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2024-11-02 21:39:08 -0300

    Java - 11.7 Ejercicio con arreglos: Ejercicio 1
    Java - 11.7 Ejercicio con arreglos: Ejercicio 1

---
+++ Tecnicatura/java/Leccion9/Arreglos_Ejercicios_1/src/Arreglos_Ejercicios_1.java
@@ -0,0 +1,30 @@
+/*
+Ejercicos 1: Leer 5 numeros. guardarlos en un arreglos y mostrarlos en el mismo orden introducidos.
+ */
+
+/**
+ 
+ */
+package arreglos_ejercicios_1;
+
+import java.util.Scanner;
+
+public class Arreglos_Ejercicios_1 {
+    public static void main(String[ ] args){
+        Scanner entrada = new Scanner(System.in);
+        float[] arreglos = new float[5];
+        
+        System.out.println("Guardando los datos en el arreglo");
+        for (int i=0;i<5; i++){
+            System.out.println((i+1)+ ". Digite un numero: ");
+            arreglos[i] = entrada.nextFloat();
+        }
+        System.out.println("\nImprimir los elementos del arreglos");
+        for (float i:arreglos){
+            System.out.print(i+" ");
+        }
+        System.out.println("\n");
+        
+    }
+    
+}
